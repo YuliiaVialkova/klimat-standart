b11871b539c351d0206f57410e42dde020884723
Yuliia Vialkova <julia.zagrafova@gmail.com>
2026-01-08 16:58:10 +0200

    created main menu

--- functions.php
+++ functions.php
@@ -11,3 +11,21 @@ function klimat_standart_theme_scripts()
 
 // "Вішаємо" нашу функцію на гачок WordPress, який відповідає за завантаження скриптів
 add_action('wp_enqueue_scripts', 'klimat_standart_theme_scripts');
+
+// 2. Функція для налаштування теми (меню, підтримка лого і т.д.)
+function klimat_standart_theme_setup()
+{
+    // Реєструємо місця для меню
+    register_nav_menus(array(
+        'header_menu' => 'Меню в шапці',
+        'footer_menu' => 'Меню в підвалі'
+    ));
+
+    // До речі, тут же зазвичай додають підтримку динамічного заголовка (title tag)
+    add_theme_support('title-tag');
+
+    // І підтримку логотипу, щоб міняти його з адмінки (на майбутнє)
+    add_theme_support('custom-logo');
+}
+// Вішаємо на гачок налаштування теми
+add_action('after_setup_theme', 'klimat_standart_theme_setup');
