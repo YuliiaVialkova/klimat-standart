c46f0e7592d6b167b8c4b25a78554c5360e3cf5c
Yuliia Vialkova <julia.zagrafova@gmail.com>
2026-01-08 21:24:54 +0200

    did desktop header

--- header.php
+++ header.php
@@ -4,28 +4,29 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
     <header class="header">
-        <div class="container">
+        <div class="header__container container container--middle">
             <div class="header__inner">
-                <div class="logo">
+                <div class="header__logo logo">
                     <a href="<?php echo home_url(); ?>">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo Klimat Standart">
                     </a>
                 </div>
 
-                <nav class="main-menu">
-                    <ul>
-                        <li><a href="#">Головна</a></li>
-                        <li><a href="#">Каталог</a></li>
-                        <li><a href="#">Про нас</a></li>
-                        <li><a href="#">Контакти</a></li>
-                    </ul>
+                <nav class="header__menu main-menu">
+                    <?php
+                    wp_nav_menu([
+                        'theme_location' => 'header_menu', // Має співпадати з назвою в functions.php
+                        'container'      => false,         // Не створювати зайвих обгорток div
+                        'menu_class'     => 'main-menu__list',            // Клас для тегу <ul>, якщо треба (поки пустий)
+                        'fallback_cb'    => '__return_false', // Якщо меню не створено, нічого не виводити
+                    ]);
+                    ?>
                 </nav>
 
             </div>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<main>
-    <div class="container">
-        <h1>Тут буде контент головної сторінки</h1>
-    </div>
-</main>
-
-<?php get_footer(); ?>
