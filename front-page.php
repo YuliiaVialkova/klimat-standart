aaa6743825c5b10b78f3e7f951adcc62fb620a0d
Yuliia Vialkova <julia.zagrafova@gmail.com>
2026-01-09 16:42:21 +0200

    finished section about

--- front-page.php
+++ front-page.php
@@ -2,14 +2,34 @@
 
 <main>
     <h1 class="visually-hidden"><?php bloginfo('name'); ?></h1>
-    <section id="about" class="about">
-        <div class="about__container container container--middle">
-            <h2>ПРО КОМПАНІЮ</h2>
-            <p>Напрямком діяльності ТОВ "Клімат Стандарт Плюс" було обрано надання послуг та продаж обладнання в сфері інженерних мереж побутових та промислових приміщень.</p>
-            <p>Наше підприємство успішно працює на ринку інженерних мереж і зарекомендувало себе, як надійний партнер та компанія з динамічними темпами розвитку.</p>
-            <p>Підприємство виконує повний цикл робіт від розрахунку і підбору обладнання до гуртової та роздрібної торгівлі, а також монтажу обладнання будь якого рівня складності, гарантійного та післягарантійного сервісного обслуговування.</p>
-        </div>
-    </section>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <section id="about" class="about">
+                <div class="about__container container container--middle">
+                    <div class="about__content">
+
+                        <?php $sec_logo = get_field('about_logo');
+                        if ($sec_logo) : ?>
+                            <img src="<?php echo $sec_logo; ?>" alt="Logo Klimat Standart">
+                        <?php endif; ?>
+
+                        <h2><?php the_field('about_title'); ?></h2>
+
+                        <div class="about__text">
+                            <?php the_field('about_text'); ?>
+                        </div>
+                    </div>
+                    <div class="about__image">
+
+                        <?php $sec_image = get_field('about_image');
+                        if ($sec_image) : ?>
+                            <img src="<?php echo $sec_image; ?>" alt="image of section about">
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </section>
+
+    <?php endwhile;
+    endif; ?>
 </main>
 
 <?php get_footer(); ?>
